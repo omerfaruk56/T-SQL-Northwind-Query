099b9aa746a94dca498ffc2c084311529fb11868
Ömer Faruk Ellialtı <omerfaruk56@live.com>
2021-02-03 00:18:07 +0300

    Update sql-query1.sql

--- sql-query1.sql
+++ sql-query1.sql
@@ -1,4 +1,4 @@
 USE Northwind
-
+--Amerika Kıtası doğu eyaletleri
 Select  t.TerritoryDescription, r.RegionDescription from Territories as t inner join 
 Region as r on r.RegionID=t.RegionID where r.RegionDescription='Eastern'
